0f4da83160d1d70e00c52bc5055d44e67dbc8f82
collinskoech11 <collinskoechck34@gmail.com>
2021-08-03 14:15:34 +0300

    update contact form model

--- famotors/fasite/admin.py
+++ famotors/fasite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import ContactForm
 # Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('brand','email')
+
+admin.site.register(ContactForm, ContactAdmin)
